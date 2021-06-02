b3d74b8985d6230f800fdf539c010f637f9fb3ca
Elena Kuliashova <lena.kuleshova@tut.by>
2021-06-02 10:49:09 +0300

    Update cucumber-testng dependency in pom file

--- src/test/java/onliner/runners/TestRunner.java
+++ src/test/java/onliner/runners/TestRunner.java
@@ -3,6 +3,7 @@ package onliner.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+
 @CucumberOptions(features = "src/test/java/onliner/features", glue = "onliner/stepdefinitions")
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestRunner extends AbstractTestNGCucumberTests{
 }
